19f394cfd944f952bce77995b258e51042b8c151
Inknyto <nytosene@gmail.com>
2025-02-13 20:41:12 +0000

    updated aliases

--- arch-linux-x86-64/nvim/lua/core/new.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = ' '
--- print('leader is set to: ',vim.g.mapleader)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- Make 'gotofile (gf)' create the file if it does not exist
-vim.cmd("map gf :e <cfile><CR>")
