fffad2e79093c120fb209136de209a85650b32e6
Inknyto <nytosene@gmail.com>
2025-10-06 18:41:23 +0000

    better version of nytoshell

---
+++ retry.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#  ~/dotfiles/retry.sh :27 Sep at 08:35:28 PM
+ 
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -i attempt_num=1
+    until "$@"
+    do
+        if ((attempt_num==max_attempts))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $((attempt_num++))
+        fi
+    done
+}
